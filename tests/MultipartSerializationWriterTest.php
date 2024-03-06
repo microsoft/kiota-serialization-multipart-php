ac22f47ae71e7433f2687e97de45495f4108ad15
silaskenneth <silaskenneth1@gmail.com>
2024-03-06 15:37:24 +0300

    Add tests file.

---
+++ tests/MultipartSerializationWriterTest.php
@@ -0,0 +1,10 @@
+<?php
+
+
+use Microsoft\Kiota\Serialization\Multipart\MultipartSerializationWriter;
+use PHPUnit\Framework\TestCase;
+
+class MultipartSerializationWriterTest extends TestCase
+{
+
+}
